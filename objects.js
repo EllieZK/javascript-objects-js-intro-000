0f0f39ee6e4f11aa5f873e771c43d8522665b317
Eliana K <ekirshe@gmail.com>
2018-07-18 18:08:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
-var playlist = {Beatles: "Yellow Submarine"}
+var playlist = {Beatles: "Yellow Submarine"};
 
 function updatePlaylist(playlist, artistName, songTitle){
   return playlist['artistName']= 'songTitle'
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+  return playlist 
 }
